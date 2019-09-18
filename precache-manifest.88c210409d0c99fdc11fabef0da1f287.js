c519d15ee5c065a9b3b3a0bb7e29a32d1391136d
Stefan Dosch <sd@Stefans-MacBook-Pro.local>
2019-09-18 20:24:31 +0200

    Updates

---
+++ precache-manifest.88c210409d0c99fdc11fabef0da1f287.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93343005862120cc90315331cd73b388",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "700f28a3596f76fa1668",
+    "url": "/monsters-rolodex/static/css/main.5a5db14f.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "700f28a3596f76fa1668",
+    "url": "/monsters-rolodex/static/js/main.e9356fe4.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
