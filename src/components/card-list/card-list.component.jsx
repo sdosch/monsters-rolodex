8ad2dac36961046b687139d8301a09b018437412
Stefan Dosch <sd@Stefans-MacBook-Pro.local>
2019-09-24 12:03:07 +0200

    added team selectbox as statful component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ export const CardList = props => {
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
-        <Card key={monster.id} monster={monster} />
+        <Card key={monster.id} monster={monster} team={props.team} />
       ))}
     </div>
   );
