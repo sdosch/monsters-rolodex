457b8f47a5b0819491c637fe2b0b20aece1b971f
Stefan Dosch <sd@Stefans-MacBook-Pro.local>
2019-09-24 12:19:41 +0200

    Updates

---
+++ precache-manifest.3eead8df95e4364840750de15dcded86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaf722e804b7ba64865d70d992cde21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12f86e16bc0992f058ee",
+    "url": "/monsters-rolodex/static/css/main.ded6bb4c.chunk.css"
+  },
+  {
+    "revision": "f6fb2ab9d3997fe0b8cd",
+    "url": "/monsters-rolodex/static/js/2.71e88c82.chunk.js"
+  },
+  {
+    "revision": "12f86e16bc0992f058ee",
+    "url": "/monsters-rolodex/static/js/main.bef2bf87.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.48ca000919ee9b6be01a1c2a9a152bc2.js"
+  "/monsters-rolodex/precache-manifest.3eead8df95e4364840750de15dcded86.js"
 );
 
 self.addEventListener('message', (event) => {
